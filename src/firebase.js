3f545e65e765c0b3fb9a1a8c6c2703222b2b9ac9
Sudhanshu Singh <singh.sudhanshu121@gmail.com>
2024-06-02 17:18:50 +0530

    ver 2

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,1 @@
-import firebase from "firebase/app";
-import "firebase/analytics";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBbUXF6CaAhQdAcnyThb_MBT57lwaS5a84",
-  authDomain: "portifolio-29f0f.firebaseapp.com",
-  projectId: "portifolio-29f0f",
-  storageBucket: "portifolio-29f0f.appspot.com",
-  messagingSenderId: "434672386501",
-  appId: "1:434672386501:web:2b566b5f45c20516a7cbac",
-  measurementId: "G-6WEER8D3RK"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
